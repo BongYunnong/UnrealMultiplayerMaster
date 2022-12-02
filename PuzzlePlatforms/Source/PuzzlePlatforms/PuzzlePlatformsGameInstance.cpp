f664eea5552bcc1b4f61d6b2db5cf23931f8cb2d
yunnong <tigermon@naver.com>
2022-12-02 22:46:15 +0900

    + Host,Join Exec + Host ServerTravel + Join ClientTravel

--- PuzzlePlatforms/Source/PuzzlePlatforms/PuzzlePlatformsGameInstance.cpp
+++ PuzzlePlatforms/Source/PuzzlePlatforms/PuzzlePlatformsGameInstance.cpp
@@ -12,3 +12,22 @@ void UPuzzlePlatformsGameInstance::Init()
 {
 	UE_LOG(LogTemp, Warning, TEXT("GameInsatnce Init"));
 }
+
+void UPuzzlePlatformsGameInstance::Host()
+{
+	if(GEngine) GEngine->AddOnScreenDebugMessage(-1, 3.0f, FColor::Blue, TEXT("Exec Host"));
+
+	UWorld* World = GetWorld();
+	if (World == nullptr) return;
+	World->ServerTravel("/Game/ThirdPerson/Maps/ThirdPersonMap?listen");  
+}
+
+void UPuzzlePlatformsGameInstance::Join(const FString& Address)
+{
+	if (GEngine) GEngine->AddOnScreenDebugMessage(-1, 3.0f, FColor::Green, FString::Printf(TEXT("Exec Join : %s"), *Address));
+
+	APlayerController* PlayerController = GetFirstLocalPlayerController();
+	if (PlayerController != nullptr) {
+		PlayerController->ClientTravel(Address, TRAVEL_Absolute);
+	}
+}
